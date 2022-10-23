ff11429489fc2ba7c6358184d2a4ce6746763979
ellipsis <ellipsis2198@gmail.com>
2022-10-23 18:18:57 -0500

    add: support for several more sites, and backends

--- DMCAlinks.ps1
+++ DMCAlinks.ps1
@@ -1,45 +1,68 @@
+#
+# This is horrendous so if you try to contribute you are a saint but if you don't try you're still okay in book
+# Enjoy, Github Copilot
 Add-Type -AssemblyName System.Windows.Forms
 $username = ""
 $userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
 $debug = 0
-$maxpages = 30
+$MAX_PAGES = 30
 
 
 if ($username -eq "") { 
 $username = Read-Host "Enter the model's username"
 }
 
+$fileHosts = @{}
+
 $sites = @(
- @{"uri"="https://tsrecs.com";"subpath"="/model/$username";"pagination"="/?page=%";"minLength"=52;"requireUser"=1;"requiredText"="$username"}, 
- @{"uri"="https://tstube.net";"subpath"="/model/$username";"pagination"="/?page=%";"minLength"=52;"requireUser"=1;"requiredText"=""}, 
- @{"uri"="https://tsvideos.org";"subpath"="/model/$username";"pagination"="/?page=%";"minLength"=52;"requireUser"=1;"requiredText"=""}, 
- @{"uri"="https://recurbate.com";"subpath"="/performer/$username";"pagination"="/page/%";"minLength"=42;"requireUser"=0;"requiredText"="video="}
- #@{"uri"="https://www.camrips.net";"subpath"="/models/$username";"pagination"="/page/%";"minLength"=52;"requireUser"=1;"requiredText"=""},
- #@{"uri"="https://www.webcamrips.com";"subpath"="/models/$username";"pagination"="/page/%";"minLength"=52;"requireUser"=1;"requiredText"=""}
+ #shared backend 1
+ @{"uri"="gaywebcamblog.com";"subpath"="/performer/$username";"pagination"="/?page=%";"minLength"=52;"startingPage"=1;"externalHost"=1;"externalFormat"="/out.php?url=";"requiredText"="$username"}, #shared backend with gayrecs, tsvideos, tsrecs, tscam - we'll only get external host URLs one time per backend because it's slow
+ @{"uri"="gayrecs.com";"subpath"="/model/$username";"pagination"="/?page=%";"minLength"=46;"startingPage"=1;"externalHost"=0;"externalFormat"="/out.php?url=";"requiredText"="$username"}, #does use external host, but it's identical to gaywebcamblog
+ @{"uri"="tsrecs.com";"subpath"="/model/$username";"pagination"="/?page=%";"minLength"=52;"startingPage"=1;"externalHost"=0;"externalFormat"="/out.php?url=";"requiredText"="$username"}, #does use external host, but it's identical to gaywebcamblog
+ @{"uri"="tscam.net";"subpath"="/performer/$username";"pagination"="/?page=%";"minLength"=52;"startingPage"=1;"externalHost"=0;"externalFormat"="/out.php?url=";"requiredText"="$username"}, #does use external host, but it's identical to gaywebcamblog
+ @{"uri"="tsvideos.org";"subpath"="/model/$username";"pagination"="/?page=%";"minLength"=52;"startingPage"=1;"externalHost"=0;"externalFormat"="/out.php?url=";"requiredText"=""}, #does use external host, but it's identical to gaywebcamblog
+ #shared backend 2
+ @{"uri"="savemycam.com";"subpath"="/$username";"pagination"="?page=%";"minLength"=42;"startingPage"=1;"externalHost"=0;"externalFormat"="";"requiredText"="$username"}, 
+ @{"uri"="cbcamsclub.com";"subpath"="/$username";"pagination"="?page=%";"minLength"=42;"startingPage"=1;"externalHost"=0;"externalFormat"="";"requiredText"="$username"}, 
+ @{"uri"="freecinemaclub.com";"subpath"="/$username";"pagination"="?page=%";"minLength"=42;"startingPage"=1;"externalHost"=0;"externalFormat"="";"requiredText"="$username"}, 
+ @{"uri"="webcamrecs.com";"subpath"="/$username";"pagination"="?page=%";"minLength"=42;"startingPage"=1;"externalHost"=0;"externalFormat"="";"requiredText"="$username"}, 
+ #misc
+ @{"uri"="camstube.me";"subpath"="/tag/$username";"pagination"="/%";"minLength"=52;"startingPage"=0;"externalHost"=0;"externalFormat"="";"requiredText"="video/$username"},
+ @{"uri"="tstube.net";"subpath"="/model/$username";"pagination"="/?page=%";"minLength"=52;"startingPage"=1;"externalHost"=0;"externalFormat"="";"requiredText"=""}, 
+ @{"uri"="webcamrips.to";"subpath"="/tag/$username";"pagination"="/?page=%";"minLength"=26;"startingPage"=1;"externalHost"=0;"externalFormat"="";"requiredText"="$username"}, 
+ @{"uri"="recurbate.com";"subpath"="/performer/$username";"pagination"="/page/%";"minLength"=42;"startingPage"=1;"externalHost"=0;"externalFormat"="";"requiredText"="video="}
+ #@{"uri"="www.camrips.net";"subpath"="/models/$username";"pagination"="/page/%";"minLength"=52;"requiredText"=""},
+ #@{"uri"="www.webcamrips.com";"subpath"="/models/$username";"pagination"="/page/%";"minLength"=52;"requiredText"=""}
 )
 
 
-function getLinks($uri, $subpath="", $pagination="", $minLength=52, $requireUser=1, $requiredText="", $sesh="") {
-  $count = 1
+function getLinks($uri, $subpath="", $pagination="", $minLength=52, $startingPage=1, $requiredText="", $externalHost=0, $externalFormat="", $maxpages=$MAX_PAGES, $includeDMCA=1, $sesh="") {
+  $count = $startingPage
   $urlList = [System.Collections.ArrayList]::new()
   while ($count -ne -1) {
     $builtUri = $uri + $subpath
     if ($debug) { Write-Host "On page $count" }
-    if ($pagination -ne "" -And $count -gt 1) {
+    if ($pagination -ne "" -And $count -gt -1) {
+      if ($debug) { Write-Host "Pagination enabled" }
       $builtUri = $builtUri + $pagination.Replace("%",$count)
     }
+    if ($debug) { Write-Host "Sending request to $builtUri" }
     $request = try { Invoke-WebRequest -Uri $builtUri -UserAgent $userAgent -WebSession $sesh -UseBasicParsing
     } catch [System.Net.WebException] {
+      if ($debug) { Write-Host $($_ | Out-String) }
       $count = -1
+      if ($debug) { Write-Host "Generic request exception. Skipping to next site." }
       continue
     }
     if ($request.StatusCode -ne 200) {
       $count = -1
+      if ($debug) { Write-Host "Non-successful code returned from webserver. Skipping to next site." }
       continue
     }
     if ($debug) { Write-Host "Successful request to $builtUri" }
+    if ($debug) { Write-Host "Links: $($request.Links.href)" }
     $urls = $request.Links.Href | Get-Unique
-    if ($debug) { Write-Host "$urls" }
+    if ($debug) { Write-Host "urls: $urls" }
     if (!($urls.Where({ $_.Contains("$username")}, 'First'))) {
       if ($debug) { Write-Host "Page does not contain $username" }
       $count = -1
@@ -52,35 +75,71 @@ function getLinks($uri, $subpath="", $pagination="", $minLength=52, $requireUser
       else {
         $workingUrl = $url
       }
+      if ($debug) { Write-Host "current url: $workingUrl" }
+      if ($includeDMCA -And $workingUrl.Contains("dmca")) { $urlList.Add($workingUrl) > $null }
       if ($workingUrl.Length -ge $MinLength) {
         if ($requiredText -eq "" -Or $workingUrl.Contains($requiredText)) {
+            if ($debug) { Write-Host "accepted url: $workingUrl" }
             $urlList.Add($workingUrl) > $null
         }
       }
     }
-    if ($count -eq $maxpages) {
+    if ($count -eq $maxpages -Or $pagination -eq "") {
+      if ($debug) { Write-Host "Reached $maxpages pages or pagination not enabled. Continuing." }
       $count =-1
       continue
     }
     $count++ 
   }
   Start-Sleep -Milliseconds (380..2800 | Get-Random)
+  if ($externalHost) {
+    $extUrlList = [System.Collections.ArrayList]::new()
+    $baseUrl = ""
+    if ($debug) { Write-Host "Getting filehost links for DMCA of offending file" }
+    ForEach ($url in $urlList) {
+        $result = getLinks -uri $url -requiredText $externalFormat -maxpages 1 -includeDMCA 0 -sesh $sesh
+        if ($debug) { Write-Host "Found matching URL: [$result]" }
+        $b64str = $result.Substring($($result.IndexOf($externalFormat) + $externalFormat.Length))
+        $decodedUrl = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($b64str))
+        $decodedUrl = $decodedUrl.Substring(0,$($decodedUrl.IndexOf("?site=")))
+        if ($debug) { Write-Host "Decoded URL: [$decodedUrl]" }
+        $baseUrl = $decodedUrl | Select-String -Pattern "https:\/\/([\w\d]+)\." | ForEach-Object {$_.Matches.Groups[1].value}
+        if ($debug) { Write-Host "External host base name: $baseUrl" }
+        $extUrlList.Add($decodedUrl) > $null
+    }
+    if ($debug) { Write-Host "External host URLs: [$extUrlList]" }
+    if (!$fileHosts.ContainsKey($baseUrl)) {$fileHosts[$baseUrl] = [System.Collections.ArrayList]::new()}
+    $extUrlList | Sort-Object -unique | ForEach-Object{$fileHosts[$baseUrl].Add($_)}
+  }
   if ($urlList.Count -eq 0) {return -1}
   return ($urlList | Sort-Object -unique)
 }
 
 ForEach ($site in $sites) {
   Invoke-WebRequest -Uri $site.item("uri") -UserAgent $userAgent -SessionVariable session -UseBasicParsing
-  $result = getLinks -uri $site.item("uri") -subpath $site.item("subpath") -pagination $site.item("pagination") -minLength $site.item("minLength") -requireUser $site.item("requireUser") -requiredText $site.item("requiredText") -sesh $session
+  $result = getLinks -uri $site.item("uri") -subpath $site.item("subpath") -pagination $site.item("pagination") -minLength $site.item("minLength") -requiredText $site.item("requiredText") -startingPage $site.item("startingPage") -externalHost $site.item("externalHost") -externalFormat $site.item("externalFormat") -sesh $session
   if ($result -eq -1) {continue}
   Write-Host ($result -join "`r`n")
   Start-Sleep -Milliseconds (2000..3800 | Get-Random)
   $SaveFileBrowser = New-Object System.Windows.Forms.SaveFileDialog -Property @{ 
     InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    FileName = "$($username)_$($site.item('uri'))_$(Get-Date -UFormat '%Y%m%d').txt"
     Filter = 'txt files (*.txt)|*.txt|All files (*.*)|*.*'
     Title = "Choose where to save the output for $($site.item('uri'))"
   }
   $null = $SaveFileBrowser.ShowDialog()
   $output = $SaveFileBrowser.FileName
   ($result -join "`r`n") > $output
+}
+ForEach ($filehost in $fileHosts.Keys) {
+   $data = $fileHosts[$filehost] | Sort-Object -Unique
+   $SaveFileBrowser = New-Object System.Windows.Forms.SaveFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    FileName = "$($username)_$($filehost)_$(Get-Date -UFormat '%Y%m%d').txt"
+    Filter = 'txt files (*.txt)|*.txt|All files (*.*)|*.*'
+    Title = "Choose where to save the output for $($filehost)"
+  }
+  $null = $SaveFileBrowser.ShowDialog()
+  $output = $SaveFileBrowser.FileName
+  ($data -join "`r`n") > $output
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # DMCA Link Grabber
-This tool (for Windows users) is meant for cammers to generate a list of links for DMCA purposes from the sites that have automated recording and publishing of shows. 
+This tool (for Windows users) is meant for cammers to generate a list of links for DMCA takedown purposes from the sites that have automated recording and publishing of shows. 
 
 ## Requirements
 You need to enable execution of unsigned PowerShell scripts on your system to run this. This will mean that any downloaded PowerShell script will be able to run, and this has the same implication as being able to run any random `.exe` file you come across. If you are not comfortable with this, that's perfectly fine - feel free to ask more questions about it or simply do not use it.
@@ -17,9 +17,22 @@ You need to enable execution of unsigned PowerShell scripts on your system to ru
  5. For each site where videos were found, a Save dialog will open - enter the desired name for the `.txt` document containing the offending links and save it. Repeat as necessary.
 
  ## Requesting support (e.g. for another site be added)
-[Submit your request here](https://github.com/ellipsis2198/dmca-link-grabber/issues/new) with as many details as possible.
- ## Currently supported sites
+[Submit your request here](https://github.com/ellipsis2198/dmca-link-grabber/issues/new) with as many details as possible. *Please* make an issue if you've found a similar site that has your copyrighted content on it, and I'll try to add support quickly.
+## Currently supported sites
+### Misc
   * `recurbate.com`
-  * `tsrecs.com`
+  * `camstube.me`
+  * `webcamrips.to`
   * `tstube.net`
+### Shared backend #1
+  * `savemycam.com`
+  * `cbcamsclub.com`
+  * `freecinemaclub.com`
+  * `webcamrecs.com`
+### Shared backend #2
+#### These all use the same database and external filehost - the tool will get you the links from the backing filehost for a DMCA takedown, as well as the links from each of the frontends. 
+  * `tsrecs.com`
+  * `tscam.net`
   * `tsvideos.org`
+  * `gayrecs.com`
+  * `gaywebcamblog.com`
